8dd26929b57c49ecb1e2a08043f8a04214ac4fc8
Minibruks <35571845+Minibruks@users.noreply.github.com>
2020-08-27 22:28:42 +0300

    Update quadro_solve.c

--- quadro_solve.c
+++ quadro_solve.c
@@ -1,38 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
+#define small_int 1e-6
 
-
-int quadro_solve(double a, double b, double c, double* x1, double* x2) {
+int quadro_solve(double a, double b, double c, double *x1, double *x2)
+{
 	double D = 0;
 
-	if (a == 0 & b == 0 & c == 0) {
-    	return -1;
-    }
-    else {
-    	if (a == 0) {
-    		if (b == 0) {
-    			return 0;
-    		}
-    		else {
-    			*x1 = -c / b;
-    			return 1;
-    		}
-    	}
-    	else {
-    		D = b * b - 4 * a * c;
-	    	*x1 = (-b - sqrt(D)) / 2;
-	    	*x2 = (-b + sqrt(D)) / 2;
+	assert(x1 != NULL);
+	assert(x2 != NULL);
 
-	    	if (fabs(D) < 0.00001) {
-	    		return 1;
-	    	}
-	    	if (D > 0) {
-	    		return 2;
-	    	}
-	    	else {
-	    		return 0;
-	    	}
-    	}
-    }
+	if (a == 0 && b == 0 && c == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		if (a == 0)
+		{
+			if (b == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				*x1 = -c / b;
+				return 1;
+			}
+		}
+		else
+		{
+			D = b * b - 4 * a * c;
+			if (D < 0)
+			{
+				return 0;
+			}
+
+			*x1 = (-b - sqrt(D)) / (2 * a);
+			*x2 = (-b + sqrt(D)) / (2 * a);
+
+			if (fabs(D) < 0.00001)
+			{
+				return 1;
+			}
+			if (D > 0)
+			{
+				return 2;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
 }
