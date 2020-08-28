c0d839a545785cc8b45aa37279bc9787c677168e
Minibruks <35571845+Minibruks@users.noreply.github.com>
2020-08-28 14:49:03 +0300

    Update quadro_solve.c

--- quadro_solve.c
+++ quadro_solve.c
@@ -7,66 +7,63 @@
 /// Params description
 enum params
 {
-	a, ///< x^2 coefficient
-	b, ///< x coefficient
-	c, ///< free term
+    a, ///< x^2 coefficient
+    b, ///< x coefficient
+    c, ///< free term
 };
 
 /** \brief A function that returns the number of roots of a square equation 
-	\version 1.0.0
-	\param[in] a, b, c
-	\param[out] number of roots
-	\return -1 - infinite number of solutions, 0 - no roots, 1 - one root, 2 - two roots
+    \version 1.0.0
+    \param[in] a, b, c
+    \param[out] number of roots
+    \return -1 - infinite number of solutions, 0 - no roots, 1 - one root, 2 - two roots
  */
 
 int quadro_solve(double a, double b, double c, double *x1, double *x2)
 {
-	double D = 0;
+    double D = 0;
+    
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    
+    if (a == 0 && b == 0 && c == 0)
+    {
+        return -1;
+    } else
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                *x1 = -c / b;
+                return 1;
+            }
+        } else
+        {
+            D = b * b - 4 * a * c;
+            if (D < 0)
+            {
+                return 0;
+            }
 
-	assert(x1 != NULL);
-	assert(x2 != NULL);
+            *x1 = (-b - sqrt(D)) / (2 * a);
+            *x2 = (-b + sqrt(D)) / (2 * a);
 
-	if (a == 0 && b == 0 && c == 0)
-	{
-		return -1;
-	}
-	else
-	{
-		if (a == 0)
-		{
-			if (b == 0)
-			{
-				return 0;
-			}
-			else
-			{
-				*x1 = -c / b;
-				return 1;
-			}
-		}
-		else
-		{
-			D = b * b - 4 * a * c;
-			if (D < 0)
-			{
-				return 0;
-			}
-
-			*x1 = (-b - sqrt(D)) / (2 * a);
-			*x2 = (-b + sqrt(D)) / (2 * a);
-
-			if (fabs(D) < small_int)
-			{
-				return 1;
-			}
-			if (D > 0)
-			{
-				return 2;
-			}
-			else
-			{
-				return 0;
-			}
-		}
-	}
+            if (fabs(D) < small_int)
+            {
+                return 1;
+            }
+            if (D > 0)
+            {
+                return 2;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
 }
