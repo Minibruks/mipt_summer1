af4a22fbaf5c81851163cddef8cc903ae20bce02
Minibruks <35571845+Minibruks@users.noreply.github.com>
2020-08-29 11:18:48 +0300

    Update unit_tests.c

--- unit_tests.c
+++ unit_tests.c
@@ -3,18 +3,18 @@
 #include <assert.h>
 #include "quadro_solve.c"
 #define NO_SOLUTIONS 0
-#define INF_SOLUTIONS 1
-#define ARR_SIZE 5
+#define INF_SOLUTIONS -1
+#define ARR_SIZE 6
 
 
 int main()
 {
-    double a[ARR_SIZE] = {1, 2, 4, 1, 0};
-    double b[ARR_SIZE] = {-4, 0, 0, 2, 0};
-    double c[ARR_SIZE] = {4, -5, 4, 3, 0};
-    double x1[ARR_SIZE] = {2, -1.58, NO_SOLUTIONS, 1, 3};
-    double x2[ARR_SIZE] = {2, 1.58, NO_SOLUTIONS, 2, 4};
-    int num_of_sol[ARR_SIZE] = {1, 2, 0, 1, 2};
+    double a[ARR_SIZE] = {1, 2, 4, 1, 0, 3};
+    double b[ARR_SIZE] = {-4, 0, 0, 2, 0, 0};
+    double c[ARR_SIZE] = {4, -5, 4, 3, 0, -27};
+    double x1[ARR_SIZE] = {2, -1.58, NO_SOLUTIONS, 1, 3, -3};
+    double x2[ARR_SIZE] = {2, 1.58, NO_SOLUTIONS, 2, 4, 3};
+    int num_of_sol[ARR_SIZE] = {1, 2, 0, 1, INF_SOLUTIONS, 2};
 
     for (int i = 0; i < ARR_SIZE; i++)
     {
